ac8a870696fbbd90f49b0825a524dd38f3f16cd8
Justin <anomie7777@gmail.com>
2018-09-07 19:43:37 +0900

    PageDTO 테스트 케이스 작성과 메소스 생성

--- src/main/java/com/anomie/webservice/commons/PageDTO.java
+++ src/main/java/com/anomie/webservice/commons/PageDTO.java
@@ -1,5 +1,6 @@
 package com.anomie.webservice.commons;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PageDTO {
 	private final int DISPLAY_PAGE_SIZE = 10;
+	private Object[] PAGE_NUMBERS = null;
 	
 	private int currentPage;
 	private int totalPage;
@@ -25,7 +27,6 @@ public class PageDTO {
 	@Builder
 	public PageDTO(int currentPage, int totalPage, int numberOfElements,List<?> content, boolean isFirst, boolean isLast, boolean hasNext,
 			boolean hasPrivious) {
-		super();
 		this.currentPage = currentPage;
 		this.totalPage = totalPage;
 		this.numberOfElements = numberOfElements;
@@ -45,6 +46,14 @@ public class PageDTO {
 			this.displayLastPage = tmp * DISPLAY_PAGE_SIZE;
 			this.displayFirstPage =  displayLastPage - (DISPLAY_PAGE_SIZE-1);
 		}
+	}
+	
+	public void setPageNumbers() {
+		List<Integer> tmp = new ArrayList<>();
+		for(int i = this.displayFirstPage; i <= this.displayLastPage; i++) {
+			tmp.add(i);
+		}
+		this.PAGE_NUMBERS = tmp.toArray();
 	}
 	
 }
--- src/test/java/com/anomie/webservice/commons/TestPageDTO.java
+++ src/test/java/com/anomie/webservice/commons/TestPageDTO.java
@@ -2,18 +2,33 @@ package com.anomie.webservice.commons;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TestPageDTO {
-
+	private PageDTO result;
+	
+	@Before
+	public void 객체_생성() {
+		result = PageDTO.builder().currentPage(26).totalPage(100).build();
+		result.setDisplayPageNumbers();
+	}
+	
 	@Test
 	public void TestgetDisplayPageNumbers() {
-		PageDTO result = PageDTO.builder().currentPage(26).totalPage(100).build();
-		result.setDisplayPageNumbers();
 		assertEquals("보여줄 첫번쨰 페이지 숫자가 다릅니다.",21, result.getDisplayFirstPage());
 		assertEquals("보여줄 마지막 페이지 숫자가 다릅니다.",30, result.getDisplayLastPage());
+	}
+	
+	@Test(expected =ArrayIndexOutOfBoundsException.class )
+	public void testGetPageNumbers() {
+		result.setPageNumbers();
+		assertEquals(21, result.getPAGE_NUMBERS()[0]);
+		assertEquals(26, result.getPAGE_NUMBERS()[5]);
+		assertEquals(30, result.getPAGE_NUMBERS()[9]);
+		assertEquals(30, result.getPAGE_NUMBERS()[10]);
 	}
 }
